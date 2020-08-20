bb36aa255c14aed77f21d34b21680b39696ed220
sitich857 <502377649@qq.com>
2020-08-20 21:53:00 +0800

    add files

--- store/cart/models.py
+++ store/cart/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Cart(models.Model):
+    userid = models.ForeignKey('用户id', UserAccount, on_delete=models.CASCADE)
+    commoditysid = models.ForeignKey('商品id', CommodityInfo, on_delete=models.CASCADE)
+    count = models.IntegerField('商品数量', default=1)
+
+    class Meta:
+        db_table = 'cart'
+        varbose_name = '购物车列表'
+        varbose_name_plural = varbose_name
